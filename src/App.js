afc6ad7bb23af4e5d8de77df0d3253ff137138f6
John S <johnc.sime@gmail.com>
2022-01-27 13:14:37 -0500

    added new feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>John's Big Money Making Machine</h1>
-      <button>Deposit 10,000,000 to my bank account. Thanks!</button>
+      <p>This is a subheading.</p>
+      <button>Deposit $1</button>
     </div>
   );
 }
